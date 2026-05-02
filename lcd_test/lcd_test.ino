bf2c972c9c65432a1fe958cb1874141ae0218af7
Adex254 <aderinsolaademola781@gmail.com>
2026-05-02 06:46:25 +0100

    Update lcd_test.ino

--- lcd_test/lcd_test.ino
+++ lcd_test/lcd_test.ino
@@ -1,18 +1,21 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 void setup() {
   Wire.begin(21, 22);
   lcd.init();
-  lcd.backlight();
-
-  lcd.setCursor(0, 0);
+  
+  // Blink the backlight 3 times to test communication
+  for(int i = 0; i < 3; i++) {
+    lcd.backlight();
+    delay(500);
+    lcd.noBacklight();
+    delay(500);
+  }
+  lcd.backlight(); // Leave it on
   lcd.print("ADEOYE");
-
-  lcd.setCursor(0, 1);
-  lcd.print("ESP32 READY");
 }
 
 void loop() {}
